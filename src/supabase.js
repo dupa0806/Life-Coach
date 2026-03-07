0a2682a7996af30ffe1b69b924b0fdbf938a8fa9
dupa0806 <dupa0806@gmail.com>
2026-03-07 16:55:29 -0500

    Add src/supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://lxwvbtcqpqilpxfsryns.supabase.co'
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4d3ZidGNxcHFpbHB4ZnNyeW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTE2ODYsImV4cCI6MjA4ODQ4NzY4Nn0.RbHAh0KDQRCaqb7UMhUAoNhWlyjiKaDeeGP75vs4L5w'
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const URL = import.meta.env.VITE_SUPABASE_URL || 'https://lxwvbtcqpqilpxfsryns.supabase.co'
+const KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4d3ZidGNxcHFpbHB4ZnNyeW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTE2ODYsImV4cCI6MjA4ODQ4NzY4Nn0.RbHAh0KDQRCaqb7UMhUAoNhWlyjiKaDeeGP75vs4L5w'
+export const supabase = createClient(URL, KEY)
